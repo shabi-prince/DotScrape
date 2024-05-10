3bc6c2f578a0273e0d495c967611d760625bdff8
shoaib <79337459+shabi-prince@users.noreply.github.com>
2024-05-10 11:12:37 +0500

    Create Community.cs

---
+++ Domain/Entities/Community.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Community : GenericModel
+    {
+        public string Name { get; set; }
+        [RegularExpression(@"(http|ftp|https):\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&amp;:/~\+#]*[\w\-\@?^=%&amp;/~\+#])?", ErrorMessage ="Invalid Url")]
+        public string Url { get; set; }
+    }
+}
